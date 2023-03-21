6a60539ce6686bc0752d03902e0004a6aa5f262d
tanguy <tlegalldutertre@gmail.com>
2023-03-21 09:54:39 +0100

    kldfj

---
+++ hello-world/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+
+
+<button @click="speak">speak</button>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+    
+  },
+
+  methods : {
+speak() {
+  console.log("ok")
+}
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
