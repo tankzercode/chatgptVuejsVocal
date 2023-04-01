f83d54e13ef592f7196cc3cbae9f84e09045a6ed
tanguy <tlegalldutertre@gmail.com>
2023-04-01 12:16:18 +0200

    lckj

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -5,6 +5,8 @@
 </template>
 
 <script>
+import { Configuration, OpenAIApi } from "openai";
+
 const recognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 const synth = window.speechSynthesis;
 
@@ -19,9 +21,24 @@ export default {
       parole: "",
       text: "",
       voices: synth.getVoices(),
+      conf: false,
+      openai: false,
+      response: false,
+      reponsetext: false,
+      reponseduchat: false,
     };
   },
   methods: {
+    async initgpt() {
+      this.conf = new Configuration({
+        organization: "org-3EzPJjL2pbiVK4bPCkwADi1m",
+        apiKey: "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",
+      });
+
+      this.openai = new OpenAIApi(this.conf);
+      this.response = await this.openai.listEngines();
+    },
+
     listen() {
       this.reco.start();
 
@@ -29,15 +46,30 @@ export default {
         const color = event.results[0][0].transcript;
         this.text = color;
         console.log(this.text);
-
-        let utterance = new SpeechSynthesisUtterance(this.text);
-        synth.speak(utterance);
+        this.initgpt();
+        this.askGpt(color);
       };
     },
+
+    async askGpt(color) {
+      this.reponsetext = await this.openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: color,
+        max_tokens: 120,
+        temperature: 1,
+      });
+      console.log(this.reponsetext.data.choices[0].text);
+      this.reponseduchat = this.reponsetext.data.choices[0].text;
+      this.speak();
+    },
+
+    async speak() {
+      let utterance = await new SpeechSynthesisUtterance(this.reponseduchat);
+        synth.speak(utterance);
+    }
   },
 
   mounted() {
-    console.log(this.recognition);
     this.reco = new recognition();
 
     this.parole = synth;
